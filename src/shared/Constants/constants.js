ae13c58078cf8a9a6e2f91d099dd19263556f3a7
Doan Viet Thang <74363928+ThangDoan2001@users.noreply.github.com>
2023-07-24 15:56:28 +0700

    update

--- src/shared/Constants/constants.js
+++ src/shared/Constants/constants.js
@@ -1,11 +1,11 @@
 const SERVER = "http://localhost:8000/"
 const CONTRACT_ADDRESS = "0xffb3bf586322e95745f2f072d6789776f84c98eb"
 const API_KEY = "f45e9304a597be4f.cac7bea50b8fa06ede5a0728463e7044"
-const CENTIC_SERVER = "http://localhost:8000/"
+const CRYPTOSCAN_SERVER = "http://localhost:8000/"
 
 module.exports = {
   SERVER,
   CONTRACT_ADDRESS,
   API_KEY,
-  CENTIC_SERVER
+  CRYPTOSCAN_SERVER
 } 
